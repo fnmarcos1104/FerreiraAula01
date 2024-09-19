94ab327b90def38abaa90fdec70a2fb78dae826a
fnmarcos1104 <marcosfnf@hotmail.com>
2024-09-19 19:10:56 -0300

    diretótio da aula 01

---
+++ Aula01/atividade01.py
@@ -0,0 +1,38 @@
+# Aula01
+
+# A:
+print('Hello World!!!')
+print('')
+
+# B:
+mensagem = "Algoritmos em Python."
+print(mensagem)
+print('')
+
+# C:
+nome = input('Digite o seu nome: ')
+
+print('Ola',nome+'!')
+print('')
+
+# D:
+cor_favorita = 'cinza'
+
+print("Meu nome e", nome, "e minha cor favorita e", cor_favorita+'.')
+print('')
+
+# E:
+profissao = 'Analista de suporte'
+endereco = 'Niteroi'
+
+print('Ola', nome, "sua profissao e", profissao+". E voce mora em", endereco+'.')
+print('')
+
+# F:
+idade = 22
+altura = '1,75m'
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+
---
+++ Aula01/main.py
@@ -0,0 +1,68 @@
+# print("hello world")
+# print("Antonio Marcos")
+
+# string - cadeia de caracteres
+# int - Números inteiros
+# float - Números reais
+# bool - True or False
+
+# +
+# -
+# *
+# / - 
+# % - Modulo (Resto da divisão)
+# // - parte inteira da divisão
+
+
+# ano_nascim = 1981
+# ano_atual = 2024
+
+# idade = ano_atual - ano_nascim
+# print(idade)
+
+
+#  n1 = 5
+# n2 = 2
+
+# resultado = n1 // n2
+# print(resultado)
+
+
+
+# print("Nome:", nome + idade)
+# print("Idade:", idade)
+# print(idade, nome)
+
+print("A:")
+print('Hello World')
+
+print("B:")
+apy = ("Algoritmos em Python")
+print(apy)
+
+print("C:")
+comprimento = ("Ola")
+nome = ("Joao")
+
+print(comprimento, nome)
+
+print("D:")
+nome = ('Marcos')
+cor_favorita = ("Cinza")
+
+print(nome, cor_favorita)
+
+print("E:")
+nome = ("Joao")
+profissao = ('analista de suporte')
+endereco = "Niteroi"
+
+print("Nome:", nome)
+print("Profissao:", profissao)
+print("Endereco:", endereco)
+
+
+
+
+
+
